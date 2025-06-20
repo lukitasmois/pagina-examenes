cb42bf6979a77decdfde4118b95226c1abdcd5d1
lukitasmois <lmoix85@gmail.com>
2025-06-20 11:37:32 -0300

    Agrego endpoint para asociar una materia a un usuario

--- backend/controllers/subjectsController.js
+++ backend/controllers/subjectsController.js
@@ -32,5 +32,35 @@ const getSubjets = async (req,res) =>{
     }
 }
 
-module.exports = {createSubject, getSubjets}
+const addSubject = async (req, res) =>{
+    const {id_subjetct, id} = req.body
+    try {
+
+        const subjetct = await Subject.findById(id_subjetct)
+        if(!subjetct){
+            return res.status(400).send({succes: false, message: 'Materia invalida.'})
+        }
+
+        const user = await User.findById(id)
+        if(!user){
+            return res.status(400).send({succes: false, message: 'Usuario invalido.'})
+        }
+
+        if(user.subjects.find((id) => id == id_subjetct)){
+            return res.status(400).send({succes: false, message: 'La materia ya esta en el usuario.'})
+        }
+
+        user.subjects.push(id_subjetct)
+        
+        await User.updateOne({id: user.id}, {subjects: user.subjects})
+        await user.save()
+        
+        res.status(200).send({succes: true, user: user})
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send({succes: false, message: 'Error al cargar la materia al usuario.'})
+    }
+}
+
+module.exports = {createSubject, getSubjets, addSubject}
 
--- backend/routes/subjectsRouter.js
+++ backend/routes/subjectsRouter.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {createSubject, getSubjets} = require('../controllers/subjectsController')
+const {createSubject, getSubjets, addSubject} = require('../controllers/subjectsController')
 
 router.post('/create', createSubject)
 
 router.get('/get-subjets', getSubjets)
+
+router.patch('/add-subject', addSubject)
 
 module.exports = router
