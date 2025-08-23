b4849c6c09800aa95c72f077d12199c6115bc7db
lukitasmois <lmoix85@gmail.com>
2025-08-23 19:57:41 -0300

    Agrego endpoint "getSubmissions" que devuelve las entregas realizadas por un estudiante

--- backend/controllers/submissionController.js
+++ backend/controllers/submissionController.js
@@ -59,8 +59,38 @@ const createSubmission = async (req, res) =>{
             message: 'Error al realizar la entrega.'
         })
 }
-};
+}
+
+const getSubmissions = async (req, res) =>{     
+    try {
+    const { id_student } = req.params;
+
+    const student = await User.findById(id_student)
+    
+    if (!student) {
+      return res.status(400).send({ success: false, message: 'Error al buscar el alumno.' });
+    }
+
+    const submissions = await Submission.find({
+      "student._id": id_student
+    }).sort({ dueDate: 1 });
+    return res.status(200).send({
+      success: true,
+      count: submissions.length,
+      submissions
+    });
+
+  } catch (error) {
+    console.error('Error getSubmissionByStudent:', error.message);
+    return res.status(500).send({
+      success: false,
+      message: 'Error al obtener los examenes.'
+    });
+  }
+
+}
 
 module.exports ={
-    createSubmission
+    createSubmission,
+    getSubmissions
 }
--- backend/routes/submissionRouter.js
+++ backend/routes/submissionRouter.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const {
-    createSubmission
+    createSubmission,
+    getSubmissions
 } = require('../controllers/submissionController')
 
 router.post('/create', createSubmission)
+
+router.get('/getSubmissions/:id_student', getSubmissions)
 
 module.exports = router
